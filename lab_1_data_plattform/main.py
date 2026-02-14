46d6ef074f00fe686fa8452cf20692e85524fb76
”Julius”_Falt_Skoglund <julius.falt.skoglund@stud.sti.se>
2026-02-14 21:30:13 +0100

    1-4 done

--- lab_1_data_plattform/main.py
+++ lab_1_data_plattform/main.py
@@ -1,5 +1,43 @@
 import pandas as pd 
 
-lab_df = pd.read_csv("lab_1.csv", dtype=str)
+# Reads in csv file and separetes columns by ";"
+lab_df = pd.read_csv("lab_1.csv", sep=';')
 
+print("Untouched data:")
+print(lab_df.head())
+
+# Removes whitespaces start/end, makes first letter capital
+lab_df["name"] = lab_df["name"].str.strip().str.replace(r"\s+", " ", regex=True).str.title() # Unclear if there actually is any double whitespaces
+lab_df["currency"] = lab_df["currency"].str.strip()
+
+# Fix date format 
+lab_df["created_at"] = lab_df["created_at"].str.replace("/", "-", regex=False)
+
+# convert types
+lab_df["price"] = pd.to_numeric(lab_df["price"], errors="coerce")
+lab_df["created_at"] = pd.to_datetime(lab_df["created_at"], errors="coerce") # Shows the dates that are wrong
+
+# Flagging for errors or simular 
+lab_df["missing_currency"] = lab_df["currency"].isna()
+lab_df["luxury_products"] = lab_df["price"] > 5000
+lab_df["price_zero?!"] = lab_df["price"] == 0
+
+
+# TODO - this does not avvisa omöjliga värden i kolumner 
+lab_df["invalid_price"] = lab_df["price"] < 0
+lab_df["missing_id"] = lab_df["id"].isna()
+lab_df["missing_name"] = lab_df["name"].isna()
+lab_df["invalid_date"] = lab_df["created_at"].isna()
+
+# rejected_df = lab_df[
+#     (lab_df["invalid_price"]) |
+#     (lab_df["missing_id"]) |
+#     (lab_df["missing_name"])
+# ]
+
+
+# actully removes invalid prices
+lab_df.loc[lab_df["price"] < 0, "price"] = None
+
+print("Cleaned data:")
 print(lab_df)
